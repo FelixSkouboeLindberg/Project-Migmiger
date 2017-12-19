9a2d914624bf6a8527d31838016f9f40f57044a8
STYFOX\felix <felix@hipcat.dk>
2017-12-19 10:39:13 +0100

    added navbar

--- index.php
+++ index.php
@@ -3,5 +3,8 @@
 		<link rel="stylesheet" type="text/css" href="stylesheet.css">
 	</head>
 	<body>
+		<ul class="navbar">
+			<a class="activ" href="index.php">Frontpage</a>
+		</ul>
 	</body>
 </html>
