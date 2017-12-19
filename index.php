bc3e489275920c5533fdb23c17d738b735585a80
STYFOX\felix <felix@hipcat.dk>
2017-12-19 11:42:35 +0100

    added loginspot

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+require('connect_mysql.php');
+?>
 <html>
 	<head>
 		<link rel="stylesheet" type="text/css" href="stylesheet.css">
@@ -6,8 +10,18 @@
 		<div class="navbar">
 			<ul>
 				<li><a class="active" href="index.php">Frontpage</a></li>
-				<li><a href="login.php">Login</a></li>
 			</ul>
+		</div>
+		<div class="loginspot">
+			<?php
+			if(isset($_SESSION['username']))
+			{
+				$username = $_SESSION['username'];
+				echo "Welcome " .$username. "<br> <a href='logout.php'>Logout?</a>";
+			} else {
+				echo "<a href='login.php'>Login</a>";
+			}
+			?>
 		</div>
 	</body>
 </html>
