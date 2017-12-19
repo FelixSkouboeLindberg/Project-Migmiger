6c28e6dd45406fd0fee2a14257991f22f29773bd
STYFOX\felix <felix@hipcat.dk>
2017-12-19 12:52:04 +0100

    css trial and error #1

--- index.php
+++ index.php
@@ -9,23 +9,29 @@ require('connect_mysql.php');
 	</head>
 	<body>
 		<div class="header">
-			<p>GemmeGag</p>
+			<div class="logo">
+				<p>GemmeGag</p>
+			</div>
+			
+			<div class="loginspot">
+				<?php
+				if(isset($_SESSION['username']))
+				{
+					$username = $_SESSION['username'];
+					echo "Welcome " .$username. "<br> <a href='logout.php'>Logout?</a>";
+				} else {
+					echo "<a href='loginpage.php'>Login</a><br>Dont have an account? <a href='registerpage.php'>Signup</a>";
+				}
+				?>
+			</div>
 		</div>
+		
+		<div class="divider"></div>
+		
 		<div class="navbar">
 			<ul>
 				<li><a class="active" href="index.php">Frontpage</a></li>
 			</ul>
-		</div>
-		<div class="loginspot">
-			<?php
-			if(isset($_SESSION['username']))
-			{
-				$username = $_SESSION['username'];
-				echo "Welcome " .$username. "<br> <a href='logout.php'>Logout?</a>";
-			} else {
-				echo "<a href='loginpage.php'>Login</a><br>Dont have an account? <a href='registerpage.php'>Signup</a>";
-			}
-			?>
 		</div>
 	</body>
 </html>
