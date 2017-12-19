3f0e4d21047f78a9f4673c69bcd9f0f2d70d7001
Danny Haslund <dannyhasl1@gmail.com>
2017-12-19 11:45:31 +0100

    tilføjer registreing

---
+++ registerpage.php
@@ -0,0 +1,8 @@
+
+
+<form action="register.php" method="POST">
+	Username: <input type="text" name="username" required> <br>
+	Password: <input type="password" name="password" required> <br>
+	Confirm Password: <input type="password" name="password2" required> <br>
+	<input type="submit" value="Register">
+</form>
