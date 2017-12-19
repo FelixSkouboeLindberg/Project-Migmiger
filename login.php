2a7f2d806cc121645dc719f87c3a9a329565b9b3
STYFOX\felix <felix@hipcat.dk>
2017-12-19 11:57:19 +0100

    removed if statement

--- login.php
+++ login.php
@@ -19,7 +19,5 @@ if (isset($_POST['username']) and isset($_POST['password'])){
 	}
 }
 //3.1.4 if the user is logged in Greets the user with message
-if (isset($_SESSION['username'])){
-	header('Location: index.php');
-}
+header('Location: index.php');
 ?>
