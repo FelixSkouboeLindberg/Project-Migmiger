a185ea8e1b980c003943b8fdda071c992b03f6e7
STYFOX\felix <felix@hipcat.dk>
2017-12-20 10:29:09 +0100

    Made showpost site

--- showpost.php
+++ showpost.php
@@ -1,4 +1,70 @@
 <?php
-	$title = $_POST["id"];
-	echo $title;
+session_start();
+require('connect_mysql.php');
 ?>
+<html>
+	<head>
+		<title>GemmeGag</title>
+		<link rel="stylesheet" type="text/css" href="stylesheet.css">
+	</head>
+	<body>
+		<div class="header">
+			<div class="logo">
+				<p>GemmeGag</p>
+			</div>
+			
+			<div class="loginspot">
+				<?php
+				if(isset($_SESSION['username']))
+				{
+					$username = $_SESSION['username'];
+					echo "Welcome " .$username. "<br> <a href='logout.php'>Logout?</a>";
+				} else {
+					echo "<a href='loginpage.php'>Login</a><br>Dont have an account? <a href='registerpage.php'>Signup</a>";
+				}
+				?>
+			</div>
+		</div>
+		
+		<div class="divider"></div>
+		
+		<div class="navbar">
+			<ul>
+				<li><a href="index.php">Frontpage</a></li>
+			</ul>
+		</div>
+		
+		<div class="content">
+			<?php
+			$error = false;
+			if($stmt = $conn->prepare("SELECT title, billede, votes FROM posts WHERE id=? LIMIT 1"))
+			{
+				$stmt->bind_param("s", $_POST["id"]);
+				$stmt->execute();
+				$stmt->bind_result($title, $img, $votes);
+				$stmt->fetch();
+				$stmt->close();
+				
+			} else {
+				echo "Could not prepare sql statement";
+				$error = TRUE;
+			}
+			
+			if($error == false)
+			{ 
+				$post = '<ul class="posts">';
+				$post .= <<<EOT
+				<li class="post">
+				<div class='post_title'><h1>{$title}</h1></div>
+				<div class="post_img"><img src="uploads/{$img}"></div>
+				<div class="post_votes">{$votes}</div>
+				<div class="comment_button">Comments</div>
+				</li>
+EOT;
+				$post .= '</ul>';
+				echo $post;
+			}
+			?>
+		</div>
+	</body>
+</html>
