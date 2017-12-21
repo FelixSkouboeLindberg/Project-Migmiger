e8999414fe42f82987730bc3398dccc66953e6f4
Danny Haslund <dannyhasl1@gmail.com>
2017-12-21 12:24:05 +0100

    profilepic add

---
+++ profile.php
@@ -0,0 +1,80 @@
+<?php
+session_start();
+require('connect_mysql.php');
+?>
+<html>
+	<head>
+		<title>GemmeGag</title>
+		<link rel="stylesheet" type="text/css" href="stylesheet.css">
+	</head>
+	<body>
+		<div class="header">
+			<div class="logo">
+				<p>GemmeGag</p>
+			</div>
+			
+			<div class="loginspot">
+				<?php
+				if(isset($_SESSION['username']))
+				{
+					$username = $_SESSION['username'];
+					echo "Welcome " .$username. "";
+				}
+				?>
+			</div>
+		</div>
+		
+		<div class="divider"></div>
+		
+		<div class="navbar">
+			<ul>
+				<li><a href="index.php">Frontpage</a></li>
+				<li><a href="createpostpage.php">Create post</a></li>
+				<?php
+				if(isset($_SESSION['username']))
+				{
+					$username = $_SESSION['username'];
+					echo "<li><a href='profile.php'>Profile</a></li>";
+					echo "<li><a href='logout.php'>Logout</a></li>";
+				} else {
+					echo "<li><a href='loginpage.php'>Login</a></li>";
+				}
+				?>
+			</ul>
+		</div>
+		
+		<div class="content">
+			<?php
+			$error = false;
+			if($stmt = $conn->prepare("SELECT username, profilepic FROM brugere WHERE id=? LIMIT 1"))
+			{
+				$stmt->bind_param("s", $_SESSION['id']);
+				$stmt->execute();
+				$stmt->bind_result($username, $billede);
+				$stmt->fetch();
+				$stmt->close();
+				
+			} else {
+				echo "Could not prepare sql statement";
+				$error = TRUE;
+			}
+			
+			if($error == false)
+			{ 
+				$post = '<ul class="posts">';
+				$post .= <<<EOT
+				<div class="post">
+					<li class="profileContainer">
+					<div class='username'><h1>{$username}</h1></div>
+					<div class="profilepic"><img src="ProfilePics/{$billede}"></div>
+				</div>
+				</li>
+EOT;
+				$post .= '</ul>';
+				echo $post;
+			}
+			?>
+		</div>
+		
+	</body>
+</html>
