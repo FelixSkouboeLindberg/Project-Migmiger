71b375b1e3bb5f425cbe0d11ff63c81794e09962
STYFOX\felix <felix@hipcat.dk>
2017-12-21 10:23:53 +0100

    added html

--- registerpage.php
+++ registerpage.php
@@ -1,6 +1,50 @@
-<form action="register.php" method="POST">
-	Username: <input type="text" name="username" required> <br>
-	Password: <input type="password" name="password" required> <br>
-	Confirm Password: <input type="password" name="password2" required> <br>
-	<input type="submit" value="Register">
-</form>
+<html>
+	<head>
+		<title>GemmeGag</title>
+		<link rel="stylesheet" type="text/css" href="stylesheet.css">
+	</head>
+	<body>
+		<div class="header">
+			<div class="logo">
+				<p>GemmeGag</p>
+			</div>
+			
+			<div class="loginspot">
+				<?php
+				if(isset($_SESSION['username']))
+				{
+					$username = $_SESSION['username'];
+					echo "Welcome " .$username. "<br> <a href='logout.php'>Logout?</a>";
+				} else {
+					echo "<a href='loginpage.php'>Login</a><br>Dont have an account? <a href='registerpage.php'>Signup</a>";
+				}
+				?>
+			</div>
+		</div>
+		
+		<div class="divider"></div>
+		
+		<div class="navbar">
+			<ul>
+				<li><a href="index.php">Frontpage</a></li>
+				<li><a class="active" href="createpostpage.php">Create post</a></li>
+			</ul>
+		</div>
+		
+		<div class="content">
+			<div class="signupform">
+				<form action="register.php" method="POST">
+					Username: <input type="text" name="username" required> <br>
+					Password: <input type="password" name="password" required> <br>
+					Confirm Password: <input type="password" name="password2" required> <br>
+					<input type="submit" value="Register">
+				</form>
+			</div>
+		</div>
+	</body>
+</html>
+
+
+
+
+
--- showpost.php
+++ showpost.php
@@ -55,10 +55,23 @@ require('connect_mysql.php');
 				$post = '<ul class="posts">';
 				$post .= <<<EOT
 				<li class="post">
-				<div class='post_title'><h1>{$title}</h1></div>
-				<div class="post_img"><img src="uploads/{$img}"></div>
-				<div class="post_votes">{$votes}</div>
-				<div class="comment_button">Comments</div>
+					<form action="showpost.php" method="POST">
+					<input type="hidden" name="id" value="{$obj->id}">
+					<div class='post_title'><h2>{$obj->title}</h2></div>
+					<div class="post_img"><img src="uploads/{$obj->billede}"></div>
+					
+					<div class="buttom_container">
+						<div class="comment_button"><input type="text" value="Comments"></div>
+						</form>
+						<form action="vote.php" method="POST">
+							<div class="VoteContainer">
+								<input type="hidden" name="id" value="{$obj->id}">
+								<div class="votes_upDown"><input type="submit" name="1" value=""></div>
+								<div class="post_votes">{$obj->votes}</div>
+								<div class="votes_upDown"><input type="submit" name="0" value=""></div>
+							</div>
+						</form>
+					</div>
 				</li>
 EOT;
 				$post .= '</ul>';
