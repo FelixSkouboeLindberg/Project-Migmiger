7441472e170edba226efefe17bed9c0239c9091b
STYFOX\felix <felix@hipcat.dk>
2017-12-21 08:08:23 +0100

    changed vote.php

--- vote.php
+++ vote.php
@@ -1,60 +1,64 @@
 <?php
+session_start();
 include("connect_mysql.php");
-if(isset($_POST["1"]))
-{	
-	$id = $_POST["id"];
-	if($stmt = $conn->prepare("SELECT votes FROM posts WHERE id=?"))
-	{
-		$stmt->bind_param("i", $id);
-		$stmt->execute();
-		$stmt->bind_result($votes);
-		$stmt->fetch();
-		$stmt->close();
-		
-		$votes = $votes + 1;
-		
-		if($stmt = $conn->prepare("UPDATE posts SET votes=? WHERE id=?"))
-		{
-			$stmt->bind_param("ii", $votes, $id);
-			$stmt->execute();
-			$stmt->close();
-			
-			header("Location: index.php");
-		} else {
-			echo "Could not prepare sql statement 2";
-		}
-		
-	} else {
-		echo "Could not prepare sql statement 1";
-	}
-}
-elseif(isset($_POST["0"]))
+if(isset($_SESSION['username']))
 {
-	$id = $_POST["id"];
-	if($stmt = $conn->prepare("SELECT votes FROM posts WHERE id=?"))
-	{
-		$stmt->bind_param("i", $id);
-		$stmt->execute();
-		$stmt->bind_result($votes);
-		$stmt->fetch();
-		$stmt->close();
-		
-		$votes = $votes - 1;
-		
-		if($stmt = $conn->prepare("UPDATE posts SET votes=? WHERE id=?"))
+	if(isset($_POST["1"]))
+	{	
+		$id = $_POST["id"];
+		if($stmt = $conn->prepare("SELECT votes FROM posts WHERE id=?"))
 		{
-			$stmt->bind_param("ii", $votes, $id);
+			$stmt->bind_param("i", $id);
 			$stmt->execute();
+			$stmt->bind_result($votes);
+			$stmt->fetch();
 			$stmt->close();
 			
-			header("Location: index.php");
+			$votes = $votes + 1;
+			
+			if($stmt = $conn->prepare("UPDATE posts SET votes=? WHERE id=?"))
+			{
+				$stmt->bind_param("ii", $votes, $id);
+				$stmt->execute();
+				$stmt->close();
+				
+				header("Location: index.php");
+			} else {
+				echo "Could not prepare sql statement 2";
+			}
 			
 		} else {
-			echo "Could not prepare sql statement 4";
+			echo "Could not prepare sql statement 1";
 		}
-		
-	} else {
-		echo "Could not prepare sql statement 3";
+	}
+	elseif(isset($_POST["0"]))
+	{
+		$id = $_POST["id"];
+		if($stmt = $conn->prepare("SELECT votes FROM posts WHERE id=?"))
+		{
+			$stmt->bind_param("i", $id);
+			$stmt->execute();
+			$stmt->bind_result($votes);
+			$stmt->fetch();
+			$stmt->close();
+			
+			$votes = $votes - 1;
+			
+			if($stmt = $conn->prepare("UPDATE posts SET votes=? WHERE id=?"))
+			{
+				$stmt->bind_param("ii", $votes, $id);
+				$stmt->execute();
+				$stmt->close();
+				
+				header("Location: index.php");
+				
+			} else {
+				echo "Could not prepare sql statement 4";
+			}
+			
+		} else {
+			echo "Could not prepare sql statement 3";
+		}
 	}
 }
 ?>
